c637e661113fed4d1c625314755a2abeeacb3170
hxari <hxari@proton.me>
2026-06-21 16:26:20 +0700

    [CREATE] Created src/uranite/backend/backend.hpp

---
+++ src/uranite/backend/backend.hpp
@@ -0,0 +1,26 @@
+
+//
+// @author hxAri (hxari)
+// @create 2025-02-24 15:15
+// @update 2026-06-17 20:03
+// @github https://github.com/uranite-lang/uranite
+//
+// Uranite Copyright (c) 2025 - hxAri <hxari@proton.me>
+// Uranite Licence under GNU General Public Licence v3
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
+#ifndef _URANITE_BACKEND_BACKEND_HPP_
+#define _URANITE_BACKEND_BACKEND_HPP_
+
+namespace uranite::backend {
+} // namespace uranite::backend
+
+#endif // end _URANITE_BACKEND_BACKEND_HPP_
