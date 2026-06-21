0ec9071c6b3729e669d96cb92d94f87fb9ebf2a8
hxari <hxari@proton.me>
2026-06-22 01:13:20 +0700

    [UPDATE] Updated src/uranite-crt/ffi-runtime.c

--- src/uranite-crt/ffi-runtime.c
+++ src/uranite-crt/ffi-runtime.c
@@ -1,24 +1,24 @@
 
-/*
- * @author hxAri (hxari)
- * @create 2026-06-15 03:00
- * @github https://github.com/uranite-lang/uranite
- *
- * Uranite -  *
- * Uranite Copyright (c) 2025 - hxAri <hxari@proton.me>
- * Uranite Licence under GNU General Public Licence v3
- *
- * Thin wrappers around POSIX dlfcn.h functions for Uranite's
- * dynamic library loading FFI. Links with -ldl.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
+
+//
+// @author hxAri (hxari)
+// @create 2025-02-24 15:15
+// @github https://github.com/uranite-lang/uranite
+//
+// Thin wrappers around POSIX dlfcn.h functions for Uranite's
+// dynamic library loading FFI. Links with -ldl.
+//
+// Uranite Copyright (c) 2025 - hxAri <hxari@proton.me>
+// Uranite Licence under GNU General Public Licence v3
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
 
 #include <dlfcn.h>
 #include <stdint.h>
