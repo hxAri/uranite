e1d9dd553b45dc25482f91d0da83dee670ef46fc
hxari <hxari@proton.me>
2026-07-18 21:32:21 +0700

    [UPDATE] Updated src/uranite/ir/mir-borrow-checker.cpp

--- src/uranite/ir/mir-borrow-checker.cpp
+++ src/uranite/ir/mir-borrow-checker.cpp
@@ -17,9 +17,9 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 //
 
-#include "uranite/ir/mir-borrow-checker.hpp"
-
 #include <fmt/format.h>
+
+#include "uranite/ir/mir-borrow-checker.hpp"
 
 namespace uranite::ir::mir {
 	
