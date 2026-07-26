efbe9749b12da462a4179891ddb5b163ea457c71
hxari <hxari@proton.me>
2026-07-26 16:00:33 +0700

    [UPDATE] Updated src/uranite/compiler/option.hpp

--- src/uranite/compiler/option.hpp
+++ src/uranite/compiler/option.hpp
@@ -114,9 +114,6 @@ namespace uranite::compiler {
 		/** @brief The target architecture triple for cross-compilation. */
 		std::string targetTriple;
 		
-		/** @brief Flag to use MIR-based LLVM codegen instead of AST-direct codegen. */
-		bool useMIR = false;
-
 		/** @brief Flag to enable verbose logging during the compilation process. */
 		bool verbose = false;
 		
