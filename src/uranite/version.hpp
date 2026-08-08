f87085cbf81da11a9001ad53704799ac62601e7b
hxari <hxari@proton.me>
2026-08-08 21:59:21 +0700

    [UPDATE] Updated src/uranite/version.hpp

--- src/uranite/version.hpp
+++ src/uranite/version.hpp
@@ -21,34 +21,38 @@
 
 #include <string>
 
-// Codename
 #define _URANITE_CODENAME_ "ChainReaction"
 
-// Version Compiler (Tooling)
 #ifndef _URANITE_COMPILER_VERSION_
 	#define _URANITE_COMPILER_VERSION_ "1.0.0"
 #endif
 
-// Version Language (Specification Uranite)
-#ifndef _URANITE_LANGUAGE_VERSION_
-	#define _URANITE_LANGUAGE_VERSION_ "2026.1"
-#endif
-
-// Uranite Language Description
-#ifndef _URANITE_LANGUAGE_DESCRIPTION_
-	#define _URANITE_LANGUAGE_DESCRIPTION_ "Accelerated Execution Through Quantum Precision"
+#ifndef _URANITE_C_RUNTIME_DIR_
+	#define _URANITE_C_RUNTIME_DIR_ ""
 #endif
 
 #ifndef _URANITE_GIT_HASH_
 	#define _URANITE_GIT_HASH_ "Unknown"
 #endif
 
+#ifndef _URANITE_LANGUAGE_VERSION_
+	#define _URANITE_LANGUAGE_VERSION_ "2026.1"
+#endif
+
+#ifndef _URANITE_LANGUAGE_DESCRIPTION_
+	#define _URANITE_LANGUAGE_DESCRIPTION_ "Accelerated Execution Through Quantum Precision"
+#endif
+
+#ifndef _URANITE_LLC_
+	#define _URANITE_LLC_ "/usr/lib/llvm-19/bin/llc"
+#endif
+
 #ifndef _URANITE_MODULES_DIR_
 	#define _URANITE_MODULES_DIR_ ""
 #endif
 
-#ifndef _URANITE_C_RUNTIME_DIR_
-	#define _URANITE_C_RUNTIME_DIR_ ""
+#ifndef _URANITE_OPT_
+	#define _URANITE_OPT_ "/usr/lib/llvm-19/bin/opt"
 #endif
 
 #endif // end _URANITE_VERSION_HPP_
